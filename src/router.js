f100783c21a31915019e42ecd7f881deea4dece1
typoscript <>
2024-05-28 17:18:15 +0900

    Add Routes: /user, /exercises, /friends, /search

--- src/router.js
+++ src/router.js
@@ -3,6 +3,12 @@ import Root from "./components/Root";
 import Main from "./components/module/Main";
 import Calender from "./components/calender/Calender";
 import Diary from "./components/diary/diary";
+import FriendListContainer from "./components/friend/FriendListContainer";
+import Body from "./components/module/Body";
+import FriendRequestListContainer from "./components/friend/FriendRequestList";
+import SearchContainer from "./components/search/SearchContainer";
+import ExerciseListContainer from "./components/search/ExerciseList";
+import ExerciseDetail from "./components/exercise/ExerciseDetail";
 
 const router = createBrowserRouter([
     {
@@ -19,16 +25,54 @@ const router = createBrowserRouter([
                 <h4>오류입니다~</h4>
             </>
     },{
-        paht: "/user",
+        path: "/user",
         element : <Root/>,
         children: [
             {
-                path:"/calender",
+                path:"user/calender",
                 element:<Calender/>
             },{
-                path:"/diary",
+                path:"user/diary",
                 element:<Diary/>
             }
+        ]
+    },
+    {
+        path: "/search",
+        element : <Root/>,
+        children: [
+            {
+                path:"/search",
+                element: <Body children={<SearchContainer/>} />
+            },
+        ]
+    },
+    {
+        path: "/friends",
+        element : <Root/>,
+        children: [
+            {
+                path:"/friends",
+                element: <Body children={<FriendListContainer/>} />
+            },
+            {
+                path:"/friends/requests",
+                element: <Body children={<FriendRequestListContainer/>} />
+            },
+        ]
+    },
+    {
+        path: "/exercises",
+        element : <Root/>,
+        children: [
+            {
+                path:"/exercises",
+                element: <Body children={<ExerciseListContainer/>} />
+            },
+            {
+                path:"/exercises/:index",
+                element: <Body children={<ExerciseDetail/>} />
+            },
         ]
     }
 ],{
