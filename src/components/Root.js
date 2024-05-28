ebb4654b49aabe252a99ea0b3f1a9c9207c8acf6
typoscript <>
2024-05-28 17:12:29 +0900

    Modify Root.js

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from './module/Header'
 import { Outlet } from 'react-router-dom'
 import Footer from './module/Footer'
+import { Box } from "@chakra-ui/react";
 
 export default function Root() {
     return (
-        <>
+        <Box minW="80vw" m="0 auto" p="30px">
             <Header/>
             <Outlet/>
             <Footer/>
-        </>
+        </Box>
     )
 }
