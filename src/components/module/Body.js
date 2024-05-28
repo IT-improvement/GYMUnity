75e62ba2c021332f06446f3575e8a8d60421a128
typoscript <>
2024-05-28 17:19:45 +0900

    Add Body Component

---
+++ src/components/module/Body.js
@@ -0,0 +1,14 @@
+import { Flex } from '@chakra-ui/react'
+import Sidebar from '../Sidebar';
+import Main from './Main';
+
+const Body = (props) => {
+    return (
+        <Flex minH="1000px" mt="10px" gap="10px" bgColor="gray.100" borderRadius="10px">
+            <Sidebar/>
+            {props.children}
+        </Flex>
+    );
+};
+
+export default Body;
