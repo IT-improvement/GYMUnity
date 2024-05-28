ed4813c2196694c4327685bdf2b52ee706bb0ac5
it-improvement <0719improvement@naver.com>
2024-05-28 10:04:35 +0900

    Create diaryMain js

---
+++ src/components/diary/diaryMain.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Diary from './diary'
+
+export default function DiaryMain() {
+  return (
+    <div style={{display:'flex', justifyContent:'center', backgroundColor:'rgba(0, 0, 0, 0.08)'}}>
+    <Diary/>
+    </div>
+  )
+}
