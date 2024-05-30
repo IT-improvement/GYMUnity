3c895a74f9c9ec639800c07c6b89a06d5d876285
typoscript <>
2024-05-30 15:57:07 +0900

    Add SearchSection

---
+++ src/components/search/SearchSection.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { Flex, Heading } from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
+import SearchOptionMenu from "./SearchOptionMenu";
+import ExerciseList from "./ExerciseList";
+import UserList from "./UserList";
+import FeedList from "../feed/FeedList";
+
+const categoryMap = {
+    all: "전체",
+    user: "유저",
+    feed: "게시글",
+    exercise: "운동법",
+};
+
+const SearchSection = () => {
+    const [category, setCategory] = useState(Object.keys(categoryMap)[0]);
+    const [isResultInDescOrder, setIsResultInDescOrder] = useState(true);
+    const { state } = useLocation();
+
+    const showSearchResult = () => {
+        let children;
+
+        switch (category) {
+            case "all":
+                children = <>
+                            <UserList searchQuery={state.searchQuery} />
+                            <FeedList searchQuery={state.searchQuery} isDescOrder={isResultInDescOrder}/>
+                            <ExerciseList searchQuery={state.searchQuery} isDescOrder={isResultInDescOrder}/>
+                        </>;
+                break;
+            case "user":
+                children = <UserList searchQuery={state.searchQuery}/>
+                break;
+            case "feed":
+                children = <FeedList searchQuery={state.searchQuery} isDescOrder={isResultInDescOrder}/>
+                break;
+            case "exercise":
+                children = <ExerciseList searchQuery={state.searchQuery} isDescOrder={isResultInDescOrder}/>
+                break;
+        }
+
+        return (
+            <Flex direction="column" gap="10px">
+                {children}
+            </Flex>
+        );
+    };
+
+    useEffect(() => {
+        showSearchResult();
+    }, [state.searchQuery, isResultInDescOrder]);
+
+
+    return (
+        <Flex width="100%" direction="column" p="10px" gap="10px">
+            <Heading>{categoryMap[category]} 검색</Heading>
+            <SearchOptionMenu categoryMap={categoryMap}
+                shouldShowDateSortButton={category !== "user"}
+                isResultInDescOrder={isResultInDescOrder}
+                setCategory={setCategory}
+                setIsResultInDescOrder={setIsResultInDescOrder}
+            />
+            {showSearchResult()}
+        </Flex>
+    );
+};
+
+export default SearchSection;
