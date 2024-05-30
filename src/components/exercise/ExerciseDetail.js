7eee9b0caf1550951d8ee2e1a2223b763d300278
typoscript <>
2024-05-30 15:50:50 +0900

    Modify ExerciseDetail

--- src/components/exercise/ExerciseDetail.js
+++ src/components/exercise/ExerciseDetail.js
@@ -1,36 +1,107 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { Flex, VStack, HStack, Grid, Image, Box, Button, Heading, Text } from "@chakra-ui/react";
+import { useState, useEffect, useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Flex, Grid, Box, Button, Heading, Text, useToast } from "@chakra-ui/react";
+import Context from "../../Context";
 
-const ExerciseDetail = (props) => {
+const ExerciseDetail = () => {
     const [exercise, setExercise] = useState({});
+    const { isLoggedIn, userCode } = useContext(Context);
     const { index } = useParams()
+    const navigate = useNavigate();
+    const toast = useToast();
 
-    const fetchExercise = () => { 
-        fetch(`${process.env.REACT_APP_SERVER_URL}/exercises?command=read_one&index=${index}`)
-            .then(response => response.json())
-            .then(data => setExercise(data))
-            .catch(err => {
-                return;
+    const showExerciseDeleteStatus = (isSuccess) => {
+        toast({
+            title: `${isSuccess ? "운동법 삭제 성공" : "운동법 삭제 불가"}`,
+            status: `${isSuccess ? "success" : "info"}`,
+            duration: 1500,
+            isClosable: true,
+            position: "top",
         });
+    };
+
+    const fetchExercise = () => {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/exercises?command=read_one&exercise_index=${index}`, {
+            method: "GET", 
+            headers: {
+                "Authorization": userCode,
+            }, 
+        })
+        .then(response => response.json())
+        .then(data => setExercise(data))
+        .finally(() => {
+        });
+    };
+
+    const handlePostDeleteOnClick = () => {
+        let isDeleted = false;
+
+        fetch(`${process.env.REACT_APP_SERVER_URL}/exercises?command=delete&exercise_index=${index}`, {
+            method: "POST", 
+            headers: {
+                "Authorization": userCode,
+            }, 
+        })
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data)
+            if (data.status === 200)
+                isDeleted = true;
+        })
+        .catch(err => {
+        })
+        .finally(() => {
+            showExerciseDeleteStatus(isDeleted);
+            
+            if (isDeleted)
+                navigate("/search", { state: { searchQuery: "" }});
+        })
     };
 
     useEffect(() => {
         fetchExercise();
-    }, [index]);
+    }, [index, isLoggedIn]);
 
     return (
-        <Flex>
+        <Flex width="100%" p="10px">
             {exercise ? 
-                <Flex direction="column">
+                <Flex width="100%" direction="column" bgColor="gray.50" p="10px">
                     <Heading>운동법</Heading>
-                    <Text>{exercise.userId}</Text>
-                    <Text>{exercise.userName}</Text>
-                    <Text>{exercise.categoryName}</Text>
-                    <Text>{exercise.name}</Text>
-                    <Text>{exercise.content}</Text>
-                    <Text>{exercise.createDate}</Text>
-                    <Text>{exercise.modDate}</Text>
+                    <Grid templateColumns="repeat(2, 1fr)">
+                        <Box>
+                            <Text>아이디: {exercise.userId}</Text>
+                        </Box>
+                        <Box>
+                            <Text>이름: {exercise.userName}</Text>
+                        </Box>
+
+                        <Box>
+                            <Text>카테고리: {exercise.categoryName}</Text>
+                        </Box>
+                        <Box>
+                            <Text>게시일: {exercise.createDate}</Text>
+                            <Text>수정일: {exercise.modDate}</Text>
+                        </Box>
+                        {isLoggedIn && (exercise.userCode === Number(window.sessionStorage.getItem("userCode"))) &&
+                            <Box>
+                                <Button colorScheme="blue" onClick={() => navigate(`/exercises/edit/${exercise.index}`)}>수정</Button>
+                                <Button colorScheme="red" onClick={handlePostDeleteOnClick}>삭제</Button>
+                            </Box>
+                        }
+                    </Grid>
+
+                    <Box>
+                        <Text>제목</Text>
+                        <Box p="10px" bgColor="white">
+                            <Text>{exercise.name}</Text>
+                        </Box>
+                    </Box>
+                    <Box>
+                        <Text>내용</Text>
+                        <Box p="10px" bgColor="white">
+                            <Text>{exercise.content}</Text>
+                        </Box>
+                    </Box>
                 </Flex>
                 : <Text>운동법이 존재하지 않습니다</Text>
             }
