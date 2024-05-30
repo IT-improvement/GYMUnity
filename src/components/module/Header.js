9062725a1e387467aaff2ebff4187882b2016352
typoscript <>
2024-05-30 15:48:17 +0900

    Modify Header

--- src/components/module/Header.js
+++ src/components/module/Header.js
@@ -1,10 +1,11 @@
-import { Flex, Box, Heading, Input, Button, Text } from '@chakra-ui/react'
+import { Flex, Box, Heading, Input, Button, Text, Avatar } from '@chakra-ui/react'
 import { Link, useNavigate } from "react-router-dom";
 import React, { useState, useContext } from 'react'
-import { Context } from "../../App";
+import Context from "../../Context";
 
 export default function Header() {
     const [searchQuery, setSearchQuery] = useState("");
+    const { isLoggedIn, setIsLoggedIn } = useContext(Context);
     const navigate = useNavigate();
 
     const handleSearchButtonOnClick = () => {
@@ -24,6 +25,12 @@ export default function Header() {
                     </Box>
                     <Input onChange={(e) => setSearchQuery(e.target.value)} value={searchQuery} />
                     <Button onClick={handleSearchButtonOnClick}>&#x1F50D;</Button>
+                    <Button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+                        {isLoggedIn ?
+                            <Avatar src='' size="sm" />
+                            : "로그인하기"
+                        }
+                    </Button>
                     </Flex>
                 </Heading>
             </Box>
