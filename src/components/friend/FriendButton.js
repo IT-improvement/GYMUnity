1b638cd9489f2cbd406b460ea44da800b6a9c17e
typoscript <>
2024-05-31 17:57:17 +0900

    Modify File: FriendButton

--- src/components/friend/FriendButton.js
+++ src/components/friend/FriendButton.js
@@ -1,34 +1,111 @@
 import { useState, useEffect, useContext } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { Flex, Grid, Box, Button, Heading, Text, useToast } from "@chakra-ui/react";
+import { Flex, Button, Text, } from "@chakra-ui/react";
 import Context from "../../Context";
 
 const FriendButton = () => {
-    const [friendStatus, setFriendStatus] = useState(false);
+    const [friendStatus, setFriendStatus] = useState("");
     const { isLoggedIn, userCode } = useContext(Context);
+    const userCodeOther = 1002;
 
     const friendButtonStatus = {
-        friend: "친구",
-        friendRequestSent: "친구 요청 보냄",
-        friendRequestSentCancel: "친구 요청 취소",
+        friend: "친구 삭제",
         notFriend: "친구 요청하기",
+        friendRequestSent: "친구 요청 취소",
+        friendRequestReceived: "친구 요청 승락",
     };
 
     const fetchFriendStatus = () => {
-        fetch(`${process.env.REACT_APP_SERVER_URL}/friends?command=read_relationship_status&userCodeOther=${index}`, {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/friends?command=read_relationship_status&userCodeOther=${userCodeOther}`, {
             method: "GET", 
             headers: {
                 "Authorization": userCode,
             }, 
         })
         .then(response => response.json())
-        .then(data => setFriendStatus(data))
+        .then(data => setFriendStatus(data.relationshipStatus))
         .finally(() => {
         });
     };
+
+    const fetchFriendDelete = () => {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/friends?command=delete&userCodeFriend=${userCodeOther}`, {
+            method: "POST", 
+            headers: {
+                "Authorization": userCode,
+            }, 
+        })
+        .then(() => fetchFriendStatus())
+        .finally(() => {
+        });
+    };
+
+    const fetchFriendRequestSend = () => {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/friends?command=create_friend_request&userCodeOther=${userCodeOther}`, {
+            method: "POST", 
+            headers: {
+                "Authorization": userCode,
+            }, 
+        })
+        .then(() => fetchFriendStatus())
+        .finally(() => {
+        });
+    };
+
+    const fetchFriendRequestCancel = () => {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/friends?command=delete_friend_request&userCodeOther=${userCodeOther}`, {
+            method: "POST", 
+            headers: {
+                "Authorization": userCode,
+            }, 
+        })
+        .then(() => fetchFriendStatus())
+        .finally(() => {
+        });
+    };
+
+    const fetchFriendRequestAccept = () => {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/friends?command=accept_friend_request&userCodeOther=${userCodeOther}`, {
+            method: "POST", 
+            headers: {
+                "Authorization": userCode,
+            }, 
+        })
+        .then(() => fetchFriendStatus())
+        .finally(() => {
+        });
+    };
+
+    const handleFriendRelationshipButtonOnClick = (e) => {
+        const status = e.target.value;
+
+        console.log(status)
+
+        switch (status) {
+            case Object.keys(friendButtonStatus)[0]:
+                fetchFriendDelete();
+                return;
+            case Object.keys(friendButtonStatus)[1]:
+                fetchFriendRequestSend();
+                return;
+            case Object.keys(friendButtonStatus)[2]:
+                fetchFriendRequestCancel();
+                return;
+            case Object.keys(friendButtonStatus)[3]:
+                fetchFriendRequestAccept();
+                return;
+        }
+    };
+
+    useEffect(() => {
+        fetchFriendStatus();
+    }, [userCode]);
     return (
         <Flex>
-            <Button>{friendButtonStatus.friendStatus}</Button>
+            <Text>My userCode: {userCode}</Text>
+            <Text>Other userCode: {userCodeOther}</Text>
+            <Button colorScheme="pink" onClick={handleFriendRelationshipButtonOnClick} value={friendStatus}>
+                {friendButtonStatus[friendStatus]}
+            </Button>
         </Flex>
     );
 };
