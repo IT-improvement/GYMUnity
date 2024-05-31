b84aa3c708b629b817900b7b1016c7ca0001ee54
k0gang <152839409+k0gang@users.noreply.github.com>
2024-05-31 16:13:55 +0900

    Update form

--- src/components/diet/createFoodForm.js
+++ src/components/diet/createFoodForm.js
@@ -1,111 +1,139 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Button, Input, Select, Stack } from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Box, Button, Input, Select, Stack } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 const CreateFoodForm = () => {
-    const [foodName, setFoodName] = useState('');
-    const [protein, setProtein] = useState('');
-    const [calory, setCalory] = useState('');
-    const [carbs, setCarbs] = useState('');
-    const [size, setSize] = useState('');
-    const [foodCategory, setFoodCategory] = useState('');
-    const [foodCategoryOptions, setFoodCategoryOptions] = useState([]);
-    const navigate = useNavigate();
+  const [foodName, setFoodName] = useState("");
+  const [protein, setProtein] = useState("");
+  const [calory, setCalory] = useState("");
+  const [carbs, setCarbs] = useState("");
+  const [size, setSize] = useState("");
+  const [foodCategory, setFoodCategory] = useState("");
+  const [foodCategoryOptions, setFoodCategoryOptions] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchFoodCategories = async () => {
-            try {
-                const response = await fetch(
-                    `${process.env.REACT_APP_SERVER_URL}/foodCategory/service?command=readAllFoodCategory&userCode=1001`,
-                    {
-                        method: 'GET',
-                        headers: {
-                            Authorization: window.sessionStorage.getItem('userCode'),
-                        },
-                    }
-                );
+  useEffect(() => {
+    const fetchFoodCategories = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_SERVER_URL}/foodCategory/service?command=readAllFoodCategory&userCode=1001`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: window.sessionStorage.getItem("userCode"),
+            },
+          }
+        );
 
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-
-                const data = await response.json();
-                console.log('Fetched categories:', data);
-                setFoodCategoryOptions(data);
-            } catch (error) {
-                console.error('Error fetching food categories:', error);
-            }
-        };
-
-        fetchFoodCategories();
-    }, []);
-
-    const handleCreateFood = async () => {
-        try {
-            const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/food/service?command=createFood`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: window.sessionStorage.getItem('userCode'),
-                },
-                body: JSON.stringify({
-                    userCode: '1001',
-                    foodCategoryIndex: foodCategory,
-                    foodName: foodName,
-                    protein: protein,
-                    calory: calory,
-                    carbs: carbs,
-                    size: size,
-                }),
-            });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-
-            const responseData = await response.json();
-            console.log('Food created:', responseData);
-
-            if (responseData.status === 'success') {
-                alert('Food created successfully');
-                navigate('/diet');
-            } else {
-                alert('Failed to create food');
-            }
-        } catch (error) {
-            console.error('Error creating food:', error);
-            alert('Error creating food: ' + error.message);
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
         }
+
+        const data = await response.json();
+        console.log("Fetched categories:", data);
+        setFoodCategoryOptions(data);
+      } catch (error) {
+        console.error("Error fetching food categories:", error);
+      }
     };
 
-    const navigateToCreateFoodCategoryForm = () => {
-        navigate('/diet/createFoodCategory');
-    };
+    fetchFoodCategories();
+  }, []);
 
-    return (
-        <Box>
-            <Stack spacing={3}>
-                <Input placeholder="Food Name" value={foodName} onChange={(e) => setFoodName(e.target.value)} />
-                <Input placeholder="Protein" value={protein} onChange={(e) => setProtein(e.target.value)} />
-                <Input placeholder="Calory" value={calory} onChange={(e) => setCalory(e.target.value)} />
-                <Input placeholder="Carbs" value={carbs} onChange={(e) => setCarbs(e.target.value)} />
-                <Input placeholder="Size" value={size} onChange={(e) => setSize(e.target.value)} />
-                <Select
-                    placeholder="Select food category"
-                    value={foodCategory}
-                    onChange={(e) => setFoodCategory(e.target.value)}
-                >
-                    {foodCategoryOptions.map((category) => (
-                        <option key={category.foodCategoryIndex} value={category.foodCategoryIndex}>
-                            {category.categoryName}
-                        </option>
-                    ))}
-                </Select>
-                <Button onClick={handleCreateFood}>Create Food</Button>
-                <Button onClick={navigateToCreateFoodCategoryForm}>Create Food Category</Button>
-            </Stack>
-        </Box>
-    );
+  const handleCreateFood = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_URL}/food/service?command=createFood`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: window.sessionStorage.getItem("userCode"),
+          },
+          body: JSON.stringify({
+            userCode: "1001",
+            foodCategoryIndex: foodCategory,
+            foodName: foodName,
+            protein: protein,
+            calory: calory,
+            carbs: carbs,
+            size: size,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+
+      const responseData = await response.json();
+      console.log("Food created:", responseData);
+
+      if (responseData.status === 200) {
+        alert("음식 생성 성공");
+        navigate("/diet");
+      } else {
+        alert("음식 생성 실패");
+      }
+    } catch (error) {
+      console.error("Error creating food:", error);
+      alert("Error creating food: " + error.message);
+    }
+  };
+
+  const navigateToCreateFoodCategoryForm = () => {
+    navigate("/diet/createFoodCategory");
+  };
+
+  return (
+    <Box>
+      <Stack spacing={3}>
+        <Input
+          placeholder="Food Name"
+          value={foodName}
+          onChange={(e) => setFoodName(e.target.value)}
+        />
+        <Input
+          placeholder="Protein"
+          value={protein}
+          onChange={(e) => setProtein(e.target.value)}
+        />
+        <Input
+          placeholder="Calory"
+          value={calory}
+          onChange={(e) => setCalory(e.target.value)}
+        />
+        <Input
+          placeholder="Carbs"
+          value={carbs}
+          onChange={(e) => setCarbs(e.target.value)}
+        />
+        <Input
+          placeholder="Size"
+          value={size}
+          onChange={(e) => setSize(e.target.value)}
+        />
+        <Select
+          placeholder="Select food category"
+          value={foodCategory}
+          onChange={(e) => setFoodCategory(e.target.value)}
+        >
+          {foodCategoryOptions.map((category) => (
+            <option
+              key={category.foodCategoryIndex}
+              value={category.foodCategoryIndex}
+            >
+              {category.categoryName}
+            </option>
+          ))}
+        </Select>
+        <Button onClick={handleCreateFood}>Create Food</Button>
+        <Button onClick={navigateToCreateFoodCategoryForm}>
+          Create Food Category
+        </Button>
+      </Stack>
+    </Box>
+  );
 };
 
 export default CreateFoodForm;
--- src/components/diet/diet.js
+++ src/components/diet/diet.js
@@ -1,97 +1,125 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Select, Box, Input, Stack, IconButton } from '@chakra-ui/react';
-import { CloseIcon } from '@chakra-ui/icons';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Select,
+  Box,
+  Input,
+  Stack,
+  IconButton,
+} from "@chakra-ui/react";
+import { CloseIcon } from "@chakra-ui/icons";
+import { useNavigate } from "react-router-dom";
 
 const Diet = () => {
-    const [selectedFoods, setSelectedFoods] = useState([]);
-    const [newFood, setNewFood] = useState('');
-    const [foodOptions, setFoodOptions] = useState([]);
-    const navigate = useNavigate();
+  const [selectedFoods, setSelectedFoods] = useState([]);
+  const [newFood, setNewFood] = useState("");
+  const [foodOptions, setFoodOptions] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchFoodOptions = async () => {
-            try {
-                const response = await fetch('http://localhost:8080/food/service?command=readFoodList&userCode=1001', {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json; charset=UTF-8',
-                    },
-                });
+  useEffect(() => {
+    const fetchFoodOptions = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8080/food/service?command=readFoodList&userCode=1001",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json; charset=UTF-8",
+            },
+          }
+        );
 
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
 
-                const data = await response.json();
-                console.log(`responseData : ${JSON.stringify(data)}`);
+        const data = await response.json();
+        console.log(`responseData : ${JSON.stringify(data)}`);
 
-                if (data && Array.isArray(data)) {
-                    setFoodOptions(data);
-                } else {
-                    console.error('Expected an array, but got:', data);
-                }
-            } catch (error) {
-                console.error('Error fetching food options:', error);
-            }
-        };
-
-        fetchFoodOptions();
-    }, []);
-
-    const addFoodToDiet = () => {
-        setSelectedFoods([...selectedFoods, '']);
+        if (data && Array.isArray(data)) {
+          setFoodOptions(data);
+        } else {
+          console.error("Expected an array, but got:", data);
+        }
+      } catch (error) {
+        console.error("Error fetching food options:", error);
+      }
     };
 
-    const handleFoodChange = (index, event) => {
-        const updatedFoods = [...selectedFoods];
-        updatedFoods[index] = event.target.value;
-        setSelectedFoods(updatedFoods);
-    };
+    fetchFoodOptions();
+  }, []);
 
-    const handleNewFoodChange = (event) => {
-        setNewFood(event.target.value);
-    };
+  const addFoodToDiet = () => {
+    setSelectedFoods([...selectedFoods, ""]);
+  };
 
-    const navigateToCreateFoodForm = () => {
-        navigate('/diet/createFood');
-    };
+  const handleFoodChange = (index, event) => {
+    const updatedFoods = [...selectedFoods];
+    updatedFoods[index] = event.target.value;
+    setSelectedFoods(updatedFoods);
+  };
 
-    const removeFoodFromDiet = (index) => {
-        const updatedFoods = [...selectedFoods];
-        updatedFoods.splice(index, 1);
-        setSelectedFoods(updatedFoods);
-    };
+  const handleNewFoodChange = (event) => {
+    setNewFood(event.target.value);
+  };
 
-    return (
-        <Box>
-            <Button onClick={addFoodToDiet}>Add Food</Button>
-            <Stack spacing={3} mt={4}>
-                {selectedFoods.map((food, index) => (
-                    <Box key={index} display="flex" alignItems="center">
-                        <Select placeholder="Select food" value={food} onChange={(e) => handleFoodChange(index, e)}>
-                            {foodOptions.map((option) => (
-                                <option key={option.id} value={option.name}>
-                                    {option.foodName}
-                                </option>
-                            ))}
-                        </Select>
-                        <IconButton
-                            aria-label="Remove food"
-                            icon={<CloseIcon />}
-                            onClick={() => removeFoodFromDiet(index)}
-                            ml={2}
-                        />
-                    </Box>
-                ))}
-            </Stack>
-            <Box mt={4}>
-                <Button mt={2} onClick={navigateToCreateFoodForm}>
-                    Add New Food
-                </Button>
-            </Box>
-        </Box>
-    );
+  const navigateToCreateFoodForm = () => {
+    navigate("/diet/createFood");
+  };
+
+  const navigateToViewFoodListForm = () => {
+    navigate("/diet/foodList");
+  };
+
+  const navigateToViewFoodCategoryListForm = () => {
+    navigate("/diet/foodCategoryList");
+  };
+
+  const removeFoodFromDiet = (index) => {
+    const updatedFoods = [...selectedFoods];
+    updatedFoods.splice(index, 1);
+    setSelectedFoods(updatedFoods);
+  };
+
+  return (
+    <Box>
+      <Button onClick={addFoodToDiet}>Add Food</Button>
+      <Stack spacing={3} mt={4}>
+        {selectedFoods.map((food, index) => (
+          <Box key={index} display="flex" alignItems="center">
+            <Select
+              placeholder="Select food"
+              value={food}
+              onChange={(e) => handleFoodChange(index, e)}
+            >
+              {foodOptions.map((option) => (
+                <option key={option.id} value={option.name}>
+                  {option.foodName}
+                </option>
+              ))}
+            </Select>
+            <IconButton
+              aria-label="Remove food"
+              icon={<CloseIcon />}
+              onClick={() => removeFoodFromDiet(index)}
+              ml={2}
+            />
+          </Box>
+        ))}
+      </Stack>
+      <Box mt={4}>
+        <Button mt={2} onClick={navigateToCreateFoodForm}>
+          Add New Food
+        </Button>
+        <Button mt={2} onClick={navigateToViewFoodListForm}>
+          FoodList
+        </Button>
+        <Button mt={2} onClick={navigateToViewFoodCategoryListForm}>
+          FoodCategoryList
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default Diet;
