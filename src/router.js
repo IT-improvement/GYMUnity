375c9cda2b1f4327b713274871a5287ab471b29e
k0gang <152839409+k0gang@users.noreply.github.com>
2024-05-31 16:12:42 +0900

    Add: router path

--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
-import { createBrowserRouter, } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import DiaryMain from "./components/diary/diaryMain";
 import CalenderMain from "./components/calender/CalenderMain";
-import Root from './components/Root';
-import Main from './components/module/Main';
-import Calender from './components/calender/Calender';
-import Diet from './components/diet/diet';
-import Body from './components/module/Body';
-import CreateFoodForm from './components/diet/createFoodForm';
-import CreateFoodCategoryForm from './components/diet/createFoodCategoryForm';
+import Root from "./components/Root";
+import Main from "./components/module/Main";
+import Calender from "./components/calender/Calender";
+import Diet from "./components/diet/diet";
+import Body from "./components/module/Body";
+import CreateFoodForm from "./components/diet/createFoodForm";
+import CreateFoodCategoryForm from "./components/diet/createFoodCategoryForm";
 import Diary from "./components/diary/diary";
 import FriendSection from "./components/friend/FriendSection";
 import SearchSection from "./components/search/SearchSection";
@@ -21,133 +21,150 @@ import MyPage from "./components/user/MyPage";
 import UpdateUserForm from "./components/user/UpdateUserForm";
 import LeaveForm from "./components/user/LeaveForm";
 import FeedList from "./components/feed/feedList";
+import ViewFoodListForm from "./components/diet/viewFoodListForm";
+import ViewFoodCategoryListForm from "./components/diet/ViewFoodCategoryListForm";
+import UpdateFoodCategoryForm from "./components/diet/UpdateFoodCategoryForm";
 
 const router = createBrowserRouter(
-	[
-		{
-			path: '/',
-			element: <Root />,
-			children: [
-				{
-					path: '',
-					element: <Main />,
-				},
-			],
-			errorElement: (
-				<>
-					<h4>오류입니다~</h4>
-				</>
-			),
-		},
-		{
-			path: "/user",
-        element : <Root/>,
-        children: [
-            {
-                path:"calender",
-                element:<CalenderMain/>
-            },{
-                path:"diary",
-                element:<DiaryMain/>
-            },
-            {
-                path: "/user/join",
-                element: <JoinForm />
-            },
-            {
-                path: "/user/login",
-                element: <LoginForm />
-            },
-            {
-                path: "/user/mypage",
-                element: <MyPage />
-            },
-            {
-                path: "/user/update",
-                element: <UpdateUserForm />
-            },
-            {
-                path: "/user/leave",
-                element: <LeaveForm />
-            }
-        ]
-		},
-		{
-			path: "/search",
-			element: <Root />,
-			children: [
-				{
-					path: "/search",
-					element: <Body children={<SearchSection />} />
-				},
-			]
-		},
-		{
-			path: "/friends",
-			element: <Root />,
-			children: [
-				{
-					path: "/friends",
-					element: <Body children={<FriendSection />} />
-				},
-			]
-		},
-		{
-			path: "/exercises",
-			element: <Root />,
-			children: [
-				{
-					path: "/exercises",
-					element: <Body children={<ExerciseList />} />
-				},
-				{
-					path: "/exercises/:index",
-					element: <Body children={<ExerciseDetail />} />
-				},
-				{
-					path: "/exercises/edit/:index",
-					element: <Body children={<ExerciseEdit />} />
-				},
-			]
-		},
-		{
-			path: '/diet',
-			element: <Root />,
-			children: [
-				{
-					path: '/diet',
-					element: <Body children={<Diet />} />,
-				},
-				{
-					path: '/diet/createFood',
-					element: <Body children={<CreateFoodForm />} />,
-				},
-				{
-					path: '/diet/createFoodCategory',
-					element: <Body children={<CreateFoodCategoryForm />} />,
-				},
-			],
-		},
-		{
-        path: "/feed",
-        element : <Root/>,
-        children: [
-            {
-                path:"/feed",
-                element:<FeedList/>
-            },
-            {
-                path:"/feed/:feedIndex",
-                element:<FeedDetail/>
-            },{
-                path:"diary",
-                element:<DiaryMain/>
-            }
-        ]
-    }
-],
-	{
-		basename: "/gymunity/v1"
-	}
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "",
+          element: <Main />,
+        },
+      ],
+      errorElement: (
+        <>
+          <h4>오류입니다~</h4>
+        </>
+      ),
+    },
+    {
+      path: "/user",
+      element: <Root />,
+      children: [
+        {
+          path: "calender",
+          element: <CalenderMain />,
+        },
+        {
+          path: "diary",
+          element: <DiaryMain />,
+        },
+        {
+          path: "/user/join",
+          element: <JoinForm />,
+        },
+        {
+          path: "/user/login",
+          element: <LoginForm />,
+        },
+        {
+          path: "/user/mypage",
+          element: <MyPage />,
+        },
+        {
+          path: "/user/update",
+          element: <UpdateUserForm />,
+        },
+        {
+          path: "/user/leave",
+          element: <LeaveForm />,
+        },
+      ],
+    },
+    {
+      path: "/search",
+      element: <Root />,
+      children: [
+        {
+          path: "/search",
+          element: <Body children={<SearchSection />} />,
+        },
+      ],
+    },
+    {
+      path: "/friends",
+      element: <Root />,
+      children: [
+        {
+          path: "/friends",
+          element: <Body children={<FriendSection />} />,
+        },
+      ],
+    },
+    {
+      path: "/exercises",
+      element: <Root />,
+      children: [
+        {
+          path: "/exercises",
+          element: <Body children={<ExerciseList />} />,
+        },
+        {
+          path: "/exercises/:index",
+          element: <Body children={<ExerciseDetail />} />,
+        },
+        {
+          path: "/exercises/edit/:index",
+          element: <Body children={<ExerciseEdit />} />,
+        },
+      ],
+    },
+    {
+      path: "/diet",
+      element: <Root />,
+      children: [
+        {
+          path: "/diet",
+          element: <Body children={<Diet />} />,
+        },
+        {
+          path: "/diet/createFood",
+          element: <Body children={<CreateFoodForm />} />,
+        },
+        {
+          path: "/diet/createFoodCategory",
+          element: <Body children={<CreateFoodCategoryForm />} />,
+        },
+        {
+          path: "/diet/foodList",
+          element: <Body children={<ViewFoodListForm />} />,
+        },
+        {
+          path: "/diet/foodCategoryList",
+          element: <Body children={<ViewFoodCategoryListForm />} />,
+        },
+        {
+          path: "/diet/updateFoodCategory",
+          element: <Body children={<UpdateFoodCategoryForm />} />,
+        },
+      ],
+    },
+    {
+      path: "/feed",
+      element: <Root />,
+      children: [
+        {
+          path: "/feed",
+          element: <FeedList />,
+        },
+        {
+          path: "/feed/:feedIndex",
+          element: <FeedDetail />,
+        },
+        {
+          path: "diary",
+          element: <DiaryMain />,
+        },
+      ],
+    },
+  ],
+  {
+    basename: "/gymunity/v1",
+  }
 );
 export default router;
