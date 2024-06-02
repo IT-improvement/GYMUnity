d4d2e9b5217576683f9d0b7dc001a33b730f7493
typoscript <>
2024-06-02 22:08:47 +0900

    Modify SearchOptionMenu

--- src/components/search/SearchOptionMenu.js
+++ src/components/search/SearchOptionMenu.js
@@ -1,24 +1,26 @@
 import { Flex, Select } from "@chakra-ui/react";
 
 const SearchOptionMenu = (props) => {
-    console.log(props.shouldShowDateSortButton);
     const handleFilterCategoryOnChange = (e) => {
         props.setCategory(e.target.value);
     };
 
     const handleDataSortOnChange = (e) => {
-        props.setIsResultInDescOrder(e.target.value == "true");
+        // JSON.parse() to convert String Boolean to Boolean
+        props.setIsResultInDescOrder(JSON.parse(e.target.value));
     };
 
     return (
-        <Flex w="fit-content" p="10px" bgColor="gray.50">
-            <Select onChange={handleFilterCategoryOnChange}>
-                {Object.entries(props.categoryMap).map(([type, name]) => {
-                    return <option value={type} key={type}>{name}</option>
-                })}
+        <Flex w="fit-content" p="10px" gap="10px" borderRadius="10px" bgColor="gray.200">
+            <Select bgColor="gray.50" value={props.category} onChange={handleFilterCategoryOnChange}>
+                { Object.entries(props.categoryMap).map(([type, name]) =>
+                    <option value={type} key={type} defaultChecked={props.category === type}>
+                        {name}
+                    </option>
+                )}
             </Select>
             { props.shouldShowDateSortButton &&
-                <Select onChange={handleDataSortOnChange}>
+                <Select bgColor="gray.50" onChange={handleDataSortOnChange}>
                     <option defaultChecked={props.isResultInDescOrder} value={true}>최신순</option>
                     <option value={false}>과거순</option>
                 </Select>
