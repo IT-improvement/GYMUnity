10ae9d99477b8c508f736c1ca2a6f3281a22fd67
typoscript <>
2024-06-02 22:07:32 +0900

    Modify FriendButton

--- src/components/friend/FriendButton.js
+++ src/components/friend/FriendButton.js
@@ -1,24 +1,27 @@
-import { useState, useEffect, useContext } from "react";
-import { Flex, Button, Text, } from "@chakra-ui/react";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Flex, Text } from "@chakra-ui/react";
 import Context from "../../Context";
+import Toast from "../chakra/Toast";
 
-const FriendButton = () => {
-    const [friendStatus, setFriendStatus] = useState("");
-    const { isLoggedIn, userCode } = useContext(Context);
-    const userCodeOther = 1002;
+const friendButtonStatus = {
+    friend: "친구 삭제",
+    notFriend: "친구 요청하기",
+    friendRequestSent: "친구 요청 취소",
+    friendRequestReceived: "친구 요청 승락",
+};
 
-    const friendButtonStatus = {
-        friend: "친구 삭제",
-        notFriend: "친구 요청하기",
-        friendRequestSent: "친구 요청 취소",
-        friendRequestReceived: "친구 요청 승락",
-    };
+const FriendButton = ({ userCodeOther }) => {
+    const [friendStatus, setFriendStatus] = useState("notFriend");
+    const { isLoggedIn, sessionUser } = useContext(Context);
+    const navigate = useNavigate();
+    const friendUrlPath = `${process.env.REACT_APP_SERVER_URL}/friends`;
 
     const fetchFriendStatus = () => {
-        fetch(`${process.env.REACT_APP_SERVER_URL}/friends?command=read_relationship_status&userCodeOther=${userCodeOther}`, {
+        fetch(`${friendUrlPath}?command=read_relationship_status&userCodeOther=${userCodeOther}`, {
             method: "GET", 
             headers: {
-                "Authorization": userCode,
+                "Authorization": sessionUser.code,
             }, 
         })
         .then(response => response.json())
@@ -28,82 +31,84 @@ const FriendButton = () => {
     };
 
     const fetchFriendDelete = () => {
-        fetch(`${process.env.REACT_APP_SERVER_URL}/friends?command=delete&userCodeFriend=${userCodeOther}`, {
+        fetch(`${friendUrlPath}?command=delete&userCodeFriend=${userCodeOther}`, {
             method: "POST", 
             headers: {
-                "Authorization": userCode,
+                "Authorization": sessionUser.code,
             }, 
         })
         .then(() => fetchFriendStatus())
-        .finally(() => {
-        });
+        .catch(() => Toast.showFailed("친구 삭제 실패"));
     };
 
     const fetchFriendRequestSend = () => {
-        fetch(`${process.env.REACT_APP_SERVER_URL}/friends?command=create_friend_request&userCodeOther=${userCodeOther}`, {
+        fetch(`${friendUrlPath}?command=create_friend_request&userCodeOther=${userCodeOther}`, {
             method: "POST", 
             headers: {
-                "Authorization": userCode,
+                "Authorization": sessionUser.code,
             }, 
         })
         .then(() => fetchFriendStatus())
-        .finally(() => {
-        });
+        .catch(() => Toast.showFailed("친구 요청 보내기 실패"));
     };
 
     const fetchFriendRequestCancel = () => {
-        fetch(`${process.env.REACT_APP_SERVER_URL}/friends?command=delete_friend_request&userCodeOther=${userCodeOther}`, {
+        fetch(`${friendUrlPath}?command=delete_friend_request&userCodeOther=${userCodeOther}`, {
             method: "POST", 
             headers: {
-                "Authorization": userCode,
+                "Authorization": sessionUser.code,
             }, 
         })
         .then(() => fetchFriendStatus())
-        .finally(() => {
-        });
+        .catch(() => Toast.showFailed("친구 요청 취소 실패"));
     };
 
     const fetchFriendRequestAccept = () => {
-        fetch(`${process.env.REACT_APP_SERVER_URL}/friends?command=accept_friend_request&userCodeOther=${userCodeOther}`, {
+        fetch(`${friendUrlPath}?command=accept_friend_request&userCodeOther=${userCodeOther}`, {
             method: "POST", 
             headers: {
-                "Authorization": userCode,
+                "Authorization": sessionUser.code,
             }, 
         })
         .then(() => fetchFriendStatus())
-        .finally(() => {
-        });
+        .catch(() => Toast.showFailed("친구 요청 수락 실패"));
     };
 
     const handleFriendRelationshipButtonOnClick = (e) => {
+        if (!isLoggedIn)
+            navigate("/user/login");
+
         const status = e.target.value;
 
-        console.log(status)
-
         switch (status) {
-            case Object.keys(friendButtonStatus)[0]:
+            case "friend":
                 fetchFriendDelete();
                 return;
-            case Object.keys(friendButtonStatus)[1]:
+            case "notFriend":
                 fetchFriendRequestSend();
                 return;
-            case Object.keys(friendButtonStatus)[2]:
+            case "friendRequestSent":
                 fetchFriendRequestCancel();
                 return;
-            case Object.keys(friendButtonStatus)[3]:
+            case "friendRequestReceived":
                 fetchFriendRequestAccept();
+                return;
+            default:
                 return;
         }
     };
 
     useEffect(() => {
+        if (!userCodeOther || !isLoggedIn) 
+            return;
+
         fetchFriendStatus();
-    }, [userCode]);
+    }, [sessionUser, isLoggedIn, userCodeOther]);
+
     return (
         <Flex>
-            <Text>My userCode: {userCode}</Text>
-            <Text>Other userCode: {userCodeOther}</Text>
-            <Button colorScheme="pink" onClick={handleFriendRelationshipButtonOnClick} value={friendStatus}>
+            <Button colorScheme="pink" value={friendStatus}
+                onClick={handleFriendRelationshipButtonOnClick} >
                 {friendButtonStatus[friendStatus]}
             </Button>
         </Flex>
