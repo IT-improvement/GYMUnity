24a91a705b400fd73e344dc27c449b49e9dfc4c6
typoscript <>
2024-06-02 22:05:24 +0900

    Modify Body

--- src/components/module/Body.js
+++ src/components/module/Body.js
@@ -1,10 +1,9 @@
 import { Flex } from '@chakra-ui/react'
 import Sidebar from '../Sidebar';
-import Main from './Main';
 
 const Body = (props) => {
     return (
-        <Flex alignItems="cen" minH="1000px" mt="10px" gap="10px" bgColor="gray.100" borderRadius="10px">
+        <Flex alignItems="cen" minH="1000px" gap="10px" bgColor="gray.100" borderRadius="10px">
             <Sidebar/>
             {props.children}
         </Flex>
