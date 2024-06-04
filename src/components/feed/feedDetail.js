003d0cab2938a9caeb90cb6ee582bc2b850e4924
codekingjj <jaejung0413@naver.com>
2024-06-04 17:11:36 +0900

    Edit : FeedUpdate Method

--- src/components/feed/feedDetail.js
+++ src/components/feed/feedDetail.js
@@ -1,92 +1,204 @@
-import { useState, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useParams, userNavigate } from "react-router-dom";
-import { Flex, VStack, Grid, Avatar, Image, Box, Heading, Text, Button, useToast, Toast } from "@chakra-ui/react";
-
+import Context from "../../Context";
+import LoadingSpinner from "../chakra/LoadingSpinner";
+import { useParams, useNavigate } from "react-router-dom";
+import { Badge, Flex, VStack, Grid, Avatar, Image, Box, Heading, Text, Button, useToast, Card, CardBody } from "@chakra-ui/react";
+import Toast from "../chakra/Toast";
 
 const FeedDetail = () => {
-    const [feed, setFeed] = useState({});
-    const {feedIndex} = useParams();
-    console.log(feedIndex);
-    const toast = useToast();
-    
-    const fetchFeed = () => { 
-        console.log(`${process.env.REACT_APP_SERVER_URL}/feed/${feedIndex}?command=feedDetail`)
-        fetch(`${process.env.REACT_APP_SERVER_URL}/feed/${feedIndex}?command=feedDetail`)
-            .then(response => response.json())
-            .then(data => setFeed(data))
-            .catch(err => {
-                console.log(err);
+    const [feed, setFeed] = useState();
+    const [isFetching, setIsFetching] = useState(true);
+    const { isLoggedIn, sessionUser } = useContext(Context);
+    const { feedIndex } = useParams();
+    const navigate = useNavigate();
+    console.log(useParams())
+    let params = `command=feedDetail`;
+    console.log(`${process.env.REACT_APP_SERVER_URL}/feed/${feedIndex}?${params}`);
+    console.log(sessionUser.code)
+    const fetchFeed = () => {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/feed/${feedIndex}?${params}`, {
+            method: "GET", 
+            headers: {
+                "Authorization": sessionUser.code, 
+            },
+        })
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+          setFeed(data)
+        })
+        .catch(() => {
+            setIsFetching(false);
+        })
+
+        
+        .finally(() => {
+            setIsFetching(false);
+
+            
+            
+  
+        });
+
+    }
+    console.log(feed);
+
+
+
+    const showComments = (comments) => {
+        if (!comments || comments.length === 0)
+            return;
+
+        const commentLimit = 2;
+        let commentComponents = [];
+        let index = 0;
+
+        while (index < commentLimit - 1) {
+            const comment = comments[index++];
+            commentComponents.push(<Text key={comment.feedCommentsIndex}>{comment.comment}</Text>)
+        }
+        
+        return commentComponents;
+    };
+
+    const fetchLikeButtonOnClick = (feedIndex, checkFavorite) => { 
+        const params = `command=${checkFavorite ? "feedFavoriteDelete" : "feedFavoritePlus"}`;
+
+        fetch(`${process.env.REACT_APP_SERVER_URL}/feed/${feedIndex}?${params}`, {
+            method: "GET", 
+            headers: {
+                "Authorization": sessionUser.code, 
+            },
+        })
+        .catch(() => Toast.showFailed("좋아요 처리 실패"))
+        .finally(() => {
+            fetchFeed();
+        });
+        
+    };
+
+    const handleLikeButtonOnClick = (feedIndex, checkFavorite) => {
+        if (!isLoggedIn) {
+            Toast.showInfo("로그인이 필요합니다");
+            return;
+        }
+
+        fetchLikeButtonOnClick(feedIndex, checkFavorite);
+    };
+
+    const handlePostDeleteOnClick = () => {
+        let isDeleted = false;
+
+        fetch(`${process.env.REACT_APP_SERVER_URL}/feed/${feedIndex}?command=feedDelete`, {
+            method: "POST", 
+            headers: {
+                "Authorization": sessionUser.code,
+            }, 
+        })
+        .then(response => response.json())
+        .then((data) => {
+            if (data.status === 200)
+                isDeleted = true;
+        })
+        .finally(() => {
+            Toast.show(isDeleted, "피드 삭제 성공", "피드 삭제 실패");
+            
+            if (isDeleted)
+                navigate("/feed");
         });
     };
 
     useEffect(() => {
         fetchFeed();
-    }, [feedIndex]);
-const showFeedDeleteStatus = (isSuccess) => {
-    toast({
-        title: `${isSuccess ? "피드 삭제 성공" : "피드 삭제 불가"}`,
-        status: `${isSuccess ? "success" : "info"}`,
-        duration: 1500,
-        isClosable: true,
-        position: "top",
-    })
-}
-const handlePostDeleteOnClick=() => {
-    let isDeleted = false;
-
-    fetch(`${process.env.REACT_APP_SERVER_URL}/feed/${feedIndex}?command=feedDelete`, {
-        headers: {
-            "Authorization" : 1,
-        },
-    })
-    .then(response => response.json())
-    .then((data) => {
-        console.log(data)
-        if (data.status === 200)
-            isDeleted = true;
-    })
-    .catch(err => {
-    })
-    .finally(() => {
-        showFeedDeleteStatus(isDeleted);
-    })
-}
-
-    const showFeed = () => {
-        return (
-            
-                    
-                        <Box p="10px" borderRadius="10px" bgColor="gray.300" cursor="pointer" _hover={{backgroundColor: "gray.400"}}>
-                            <Avatar src='' size="2xl"/>
-                            <VStack gap="10px">
-                                <Text>제목 : {feed.title}</Text>
-                                <Text>작성자 : {feed.userName}</Text>
-                                <Text>내용 : {feed.content}</Text>
-                                <Text>유저번호 : {feed.userCode}</Text>
-                                <Text>작성시간 : {feed.createDate}</Text>
-                                <Text>댓글 : {feed.comments}</Text>
-                                <Text>수정날짜 : {feed.modDate}</Text>
-                                <Text>아이디 : {feed.userId}</Text>
-                                
-                                <Text>{feed.favoriteCount}</Text>
-                                <Text>{feed.checkFavorite}</Text>
-                                <Button onClick={handlePostDeleteOnClick}>게시글 삭제</Button>
-                            </VStack>
-                        </Box>
-        );
-    };
-    
+    }, []);
 
     return (
-        <VStack width="100%" p="10px">
-            <Heading>피드목록</Heading>
-            
-                <Grid templateColumns="repeat(3, 1fr)" gap="30px" justifyContent="center" >
-                    {showFeed()}
-                </Grid> 
-                :
-        </VStack>
+            <div>
+                {
+                    feed ? 
+                    <Card key={feedIndex}>
+                <CardBody>
+                <Flex direction="column" p="10px" gap="10px" borderRadius="10px" bgColor="gray.300">
+                    <Link to={`/user/${feed.userCode}`} >
+                        <Card _hover={{backgroundColor: "gray.400"}}>
+                            <CardBody>
+                                <Flex gap="10px">
+                                    <Avatar src="" size="md"/>
+                                    <Flex direction="column">
+                                        <Text>{feed.userName}</Text>
+                                        <Badge fontSize="15px" colorScheme="pink">
+                                            {feed.userId}
+                                        </Badge>
+                                    </Flex>
+                                </Flex>
+                            </CardBody>
+                        </Card>
+                    </Link>
+
+                    <Flex direction="column" gap="10px"> 
+                            <Card _hover={{backgroundColor: "gray.400"}}>
+                                <CardBody>
+                                    <Flex direction="column" gap="20px">
+                                        <Box>
+                                            <Text fontWeight="bold">제목</Text>
+                                            <Text>{feed.title}</Text>
+                                        </Box>
+                                        <Box>
+                                            <Text fontWeight="bold">내용</Text>
+                                            <Text>{feed.content}</Text>
+                                        </Box>
+                                        <Box>
+                                            <Text fontWeight="bold">작성일</Text>
+                                            <Text>{feed.createDate}</Text>
+                                        </Box>
+                                    </Flex>
+                                </CardBody>
+                            </Card>
+
+                        <Card>
+                            <CardBody>
+                                <Flex justify="space-between">
+                                    <Text>좋아요: {feed.favoriteCount}</Text>
+                                    <Image src={`${process.env.PUBLIC_URL}/images/${feed.checkFavorite ? "liked.png" : "like.png"}`}
+                                    cursor="pointer"
+                                        onClick={() => handleLikeButtonOnClick(feedIndex, feed.checkFavorite)}
+                                    />
+                                </Flex>
+                            </CardBody>
+                        </Card>
+
+                         <Card>
+                            <CardBody>
+                                { feed && feed.comments && feed.comments.length > 0 ?
+                                    showComments(feed.comments) :
+                                    <Text>댓글이 없습니다</Text>
+                                }
+                            </CardBody>
+                        </Card>
+                        
+                        { isLoggedIn && (feed.userCode === Number(sessionUser.code)) &&
+                                <Flex gap="10px">
+                                    <Button colorScheme="blue"
+                                        onClick={() => navigate(`/feed/${feedIndex}/feedupdate`)}>
+                                        수정
+                                    </Button>
+                                    <Button colorScheme="red" onClick={handlePostDeleteOnClick}>삭제</Button>
+                                </Flex>
+                                }
+                        <Card>
+                        </Card>
+                    </Flex>
+                </Flex>
+            </CardBody>
+        </Card>
+
+                : <LoadingSpinner></LoadingSpinner>}
+                  
+
+            </div>
+           
     );
 };
 
--- src/components/feed/feedupdate.js
+++ src/components/feed/feedupdate.js
@@ -1,5 +1,157 @@
-const FeedUpdate = () => {
+// import { useContext, useEffect, useState } from "react";
+// import { useNavigate, useParams } from "react-router-dom";
+// import { Box, Button, Card, CardBody, Flex, Grid, Heading, Select, Text, Textarea, Input } from "@chakra-ui/react";
+// import Context from "../../Context";
+// import Toast from "../chakra/Toast";
 
-}
+// const FeedUpdate = () => {
+//     const [feed, setFeed] = useState({
+//         title: "",
+//         content: "",
+//     });
+//     const { isLoggedIn, sessionUser } = useContext(Context);
+//     const { feedIndex } = useParams()
+//     const navigate = useNavigate();
 
-export default FeedUpdate;
+//     const fetchFeed = () => {
+//         fetch(`${process.env.REACT_APP_SERVER_URL}/feed/${feedIndex}?${params}`, {
+//             method: "GET", 
+//             headers: {
+//                 "Authorization": sessionUser.code, 
+//             }, 
+//         })
+//         .then(response => response.json())
+//         .then(data => setFeed(data))
+//         .catch(() => {
+//             Toast.showFailed("운동법 불러오기 실패");
+//             navigate("/feeds");
+//         });
+//     };
+
+
+//     const fetchFeedUpdate = () => {
+//         let isUpdated = false;
+//         const params =  `title=${feed.title}&` +
+//                         `content=${feed.content}&`
+
+//         fetch(`${process.env.REACT_APP_SERVER_URL}/feed/${feed.feedIndex}?command=feedUpdate`, {
+//             method: "POST", 
+//             headers: {
+//                 "Authorization": sessionUser.code, 
+//             }, 
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data)
+//             if (data.status === 200)
+//                 isUpdated = true;
+//         })
+//         .finally(() => {
+//             Toast.show(isUpdated, "피드 수정 성공", "피드 수정 실패");
+//             navigate(`/feed/${feed.feedIndex}`);
+//         });
+//     };
+
+//     const handleFeedFieldOnChange = (e) => {
+//         setExercise(oldFeed => {
+//             return {...oldFeed, [e.target.name]: e.target.value}
+//         });
+//     };
+
+//     const handleFeedUpdateOnSubmit = (e) => {
+//         e.preventDefault();
+//         fetchFeedUpdate();
+//     };
+
+//     useEffect(() => {
+//         fetchFeed();
+//     }, [feedIndex, isLoggedIn]);
+
+//     return (
+//         <div>
+//                 {
+//                     feed ? 
+//                     <Card key={feedIndex}>
+//                 <CardBody>
+//                 <Flex direction="column" p="10px" gap="10px" borderRadius="10px" bgColor="gray.300">
+//                     <Link to={`/user/${feed.userCode}`} >
+//                         <Card _hover={{backgroundColor: "gray.400"}}>
+//                             <CardBody>
+//                                 <Flex gap="10px">
+//                                     <Avatar src="" size="md"/>
+//                                     <Flex direction="column">
+//                                         <Text>{feed.userName}</Text>
+//                                         <Badge fontSize="15px" colorScheme="pink">
+//                                             {feed.userId}
+//                                         </Badge>
+//                                     </Flex>
+//                                 </Flex>
+//                             </CardBody>
+//                         </Card>
+//                     </Link>
+
+//                     <Flex direction="column" gap="10px"> 
+//                             <Card _hover={{backgroundColor: "gray.400"}}>
+//                                 <CardBody>
+//                                     <Flex direction="column" gap="20px">
+//                                         <Box>
+//                                             <Text fontWeight="bold">제목</Text>
+//                                             <Text>{feed.title}</Text>
+//                                         </Box>
+//                                         <Box>
+//                                             <Text fontWeight="bold">내용</Text>
+//                                             <Text>{feed.content}</Text>
+//                                         </Box>
+//                                         <Box>
+//                                             <Text fontWeight="bold">작성일</Text>
+//                                             <Text>{feed.createDate}</Text>
+//                                         </Box>
+//                                     </Flex>
+//                                 </CardBody>
+//                             </Card>
+
+//                         <Card>
+//                             <CardBody>
+//                                 <Flex justify="space-between">
+//                                     <Text>좋아요: {feed.favoriteCount}</Text>
+//                                     <Image src={`${process.env.PUBLIC_URL}/images/${feed.checkFavorite ? "liked.png" : "like.png"}`}
+//                                     cursor="pointer"
+//                                         onClick={() => handleLikeButtonOnClick(feedIndex, feed.checkFavorite)}
+//                                     />
+//                                 </Flex>
+//                             </CardBody>
+//                         </Card>
+
+//                          <Card>
+//                             <CardBody>
+//                                 { feed && feed.comments && feed.comments.length > 0 ?
+//                                     showComments(feed.comments) :
+//                                     <Text>댓글이 없습니다</Text>
+//                                 }
+//                             </CardBody>
+//                         </Card>
+                        
+//                         { isLoggedIn && (feed.userCode === Number(sessionUser.code)) &&
+//                                 <Flex gap="10px">
+//                                     <Button colorScheme="blue"
+//                                         onClick={() => navigate(`/feed/${feedIndex}/feedupdate`)}>
+//                                         수정
+//                                     </Button>
+//                                     <Button colorScheme="red" onClick={handlePostDeleteOnClick}>삭제</Button>
+//                                 </Flex>
+//                                 }
+//                         <Card>
+//                         </Card>
+//                     </Flex>
+//                 </Flex>
+//             </CardBody>
+//         </Card>
+
+//                 : <LoadingSpinner></LoadingSpinner>}
+                  
+
+//             </div>
+//     );
+// };
+
+// export default ExerciseUpdate;
