ab38bbd2965322c1da4ca3cb0caeeae41f65bcec
typoscript <>
2024-06-04 17:15:43 +0900

    Add File: UserList

---
+++ src/components/user/UserList.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Avatar, AvatarGroup, Box, Flex, Grid, Heading, Text } from "@chakra-ui/react";
+import LoadingSpinner from "../chakra/LoadingSpinner";
+import Toast from "../chakra/Toast";
+import UserCard from "./UserCard";
+
+const UserList = ({ searchQuery, isTotalSearch }) => {
+    const [users, setUsers] = useState([]);
+    const [isFetching, setIsFetching] = useState(true);
+    const [userLimit] = useState(4);
+    const navigate = useNavigate();
+
+    const fetchUsers = () => { 
+        let params = searchQuery ?
+            `command=read_all_by_query&query=${searchQuery}` :
+            `command=read_all`;
+
+        if (isTotalSearch)
+            params += `&limit=${userLimit}`;
+
+        fetch(`${process.env.REACT_APP_SERVER_URL}/user?${params}`)
+        .then(response => response.json())
+        .then(data => setUsers(data))
+        .catch(() => Toast.showFailed("유저 로드 실패"))
+        .finally(() => {
+            setIsFetching(false);
+        });
+    };
+
+    useEffect(() => {
+        fetchUsers();
+    }, [searchQuery, isTotalSearch]);
+
+    const showAvatar = () => {
+        const avatars = [];
+
+        for (let i = 0; i < users.length; i++)
+            avatars.push(<Avatar key={`user_image_${i}`} src="" />);
+
+        return avatars;
+    };
+
+    const showUserMoreSection = () => {
+        return (
+            <Flex justify="right">
+                <Box p="10px" textAlign="center" bgColor="gray.200" borderRadius="10px"
+                    cursor="pointer"
+                    onClick={() => navigate("/search", { state: { searchQuery: searchQuery, category: "user" }})} >
+                        {users.length >= userLimit &&
+                        <>
+                            <AvatarGroup size="md" max={userLimit}>
+                                {showAvatar()}
+                            </AvatarGroup>
+                            <Text color="gray.600">유저 더보기</Text>
+                        </>
+                        }
+                </Box>
+            </Flex>
+        );
+    };
+
+    return (
+        <Flex direction="column" w="100%" p="10px" gap="10px" bgColor="gray.300" borderRadius="10px">
+            <Heading>유저목록</Heading>
+            { isFetching ?
+                <Flex w="100%" h="100%" justify="center" align="center" >
+                    <LoadingSpinner />
+                </Flex> :
+                users.length > 0 ? 
+                    <Flex direction="column" w="100%" p="10px" gap="10px">
+                        <Grid gridTemplateColumns={`repeat(${userLimit}, 1fr)`}
+                            w="100%" gap="10px" overflow="hidden">
+                            { users.map(user =>
+                                <UserCard user={user} />
+                            )}
+                        </Grid>
+                        { isTotalSearch && (users.length >= userLimit) && showUserMoreSection() }
+                    </Flex>
+                    :
+                    <Heading fontSize="20px">유저가 없습니다</Heading>
+            }
+        </Flex>
+    );
+};
+
+export default UserList;
