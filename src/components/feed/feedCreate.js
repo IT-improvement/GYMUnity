4b6fa6021031d8c031f1c177efd3672d89c49f96
codekingjj <jaejung0413@naver.com>
2024-06-05 10:16:24 +0900

    Add : feedUpdate

--- src/components/feed/feedCreate.js
+++ src/components/feed/feedCreate.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { useToast, Flex, Box, Select, Heading, FormControl, FormLabel, Button, Text, Input } from "@chakra-ui/react";
+import { Avatar, Badge, Card, CardBody, useToast, Flex, Box, Select, Heading, FormControl, FormLabel, Button, Text, Input } from "@chakra-ui/react";
 import Context from "../../Context";
 
 const FeedCreate = () => {
@@ -41,17 +41,41 @@ const FeedCreate = () => {
     console.log(feed)
 
     return (
-        <>
-            <form onSubmit={handleFeedCreateOnSubmit}>
-                <label>제목</label>
-                <input type="text" name="title" onChange={handleFeedFieldOnChange} />
-                <label>내용</label>
-                <input type="text" name="content" onChange={handleFeedFieldOnChange} />
+        <CardBody>
+            <Flex direction="column" p="10px" gap="10px" borderRadius="10px" bgColor="gray.300">
+                <Card _hover={{backgroundColor: "gray.400"}}>
+                    <CardBody>
+                        <Flex gap="10px">
+                            <Avatar src="" size="md"/>
+                            <Flex direction="column">
+                                <Text>{feed.userName}</Text>
+                                <Badge fontSize="15px" colorScheme="pink">
+                                    {feed.userId}
+                                </Badge>
+                            </Flex>
+                        </Flex>
+                    </CardBody>
+                </Card>
 
-                <input type="submit" value="피드 저장"/>
-                <Button colorScheme="blue" onClick={() => navigate("/feed")}>목록으로 가기</Button>
-            </form>
-        </>
+            <Flex direction="column" gap="10px"> 
+                    <Card _hover={{backgroundColor: "gray.400"}}>
+                        <CardBody>
+                            <Flex direction="column" gap="20px">
+                                <Box>
+                                    <Text fontWeight="bold">제목</Text>
+                                    <Input type="text" name="title" value={feed.title} onChange={handleFeedFieldOnChange} />
+                                </Box>
+                                <Box>
+                                    <Text fontWeight="bold">내용</Text>
+                                    <Input type="text" name="content" value={feed.content} onChange={handleFeedFieldOnChange} />
+                                </Box>
+                            </Flex>
+                        </CardBody>
+                    </Card>
+                
+            </Flex>
+        </Flex>
+    </CardBody>
     );
 };
 
