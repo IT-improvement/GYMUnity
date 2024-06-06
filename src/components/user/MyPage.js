40d92409d9ec8d3ac42ff7227da7037e1bd69477
onceuponatim9 <sjung8018a@gmail.com>
2024-06-07 01:02:10 +0900

    Update MyPage.js

--- src/components/user/MyPage.js
+++ src/components/user/MyPage.js
@@ -3,9 +3,9 @@ import ButtonLogout from "./ButtonLogout";
 
 const MyPage = () => {
     return (
-        <div style={{ height: 'auto', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{ width: '100%', height: 'auto', display: 'flex', marginTop: '100px', justifyContent: 'center' }}>
             <div>
-            <hr style={{border: '1px solid darkgray', marginTop: '15px', marginBottom: '15px'}} />
+                <hr style={{border: '1px solid darkgray', marginTop: '15px', marginBottom: '15px'}} />
                 <Link to="../update" style={{fontSize: '25px'}}>• 회원정보 수정</Link>
                 <hr style={{border: '1px solid darkgray', width: '200px', marginTop: '15px', marginBottom: '15px'}} />
 
