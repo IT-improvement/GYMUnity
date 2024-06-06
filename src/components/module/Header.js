6bb7325c565fe09f59a588aa8324852970735d4c
typoscript <>
2024-06-06 15:00:02 +0900

    Modify Header

--- src/components/module/Header.js
+++ src/components/module/Header.js
@@ -1,6 +1,6 @@
 import { useState, useContext } from "react"
 import { Link, useNavigate } from "react-router-dom";
-import { Avatar, Box, Button, Flex, Heading, Input, Text } from "@chakra-ui/react"
+import { Avatar, Box, Button, Flex, Heading, Input } from "@chakra-ui/react"
 import Context from "../../Context";
 
 export default function Header() {
@@ -25,11 +25,10 @@ export default function Header() {
             borderWidth="2px"
             onChange={(e) => setSearchQuery(e.target.value)} value={searchQuery} />
         <Button onClick={handleSearchButtonOnClick}>&#x1F50D;</Button>
-            <Button height="fit-content">
+            <Button h="fit-content" p="5px" >
                 {isLoggedIn ?
                     <Link to="/user/mypage">
                         <Avatar src="" size="sm" />
-                        <Text>{sessionUser.id}</Text>
                     </Link>
                     :
                     <Link to="/user/login">로그인</Link>
