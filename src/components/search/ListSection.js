b089ec0bf1a780aebb56ce0197ac5bb75944aaa1
typoscript <>
2024-06-06 22:11:31 +0900

    Modify ListSection

--- src/components/search/ListSection.js
+++ src/components/search/ListSection.js
@@ -2,7 +2,7 @@ import { Flex } from "@chakra-ui/react";
 
 const ListSection = (props) => {
     return (
-        <Flex direction="column" w="100%" p="10px" bgColor="gray.200" gap="10px" borderRadius="10px">
+        <Flex direction="column" w="100%" p="10px" bgColor="gray.200" gap="20px" borderRadius="10px">
             {props.children}
         </Flex>
     );
