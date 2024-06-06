55add36a8745c8d3d222ea6e14ce6e5f399a4e43
typoscript <>
2024-06-06 14:58:29 +0900

    Modify ExerciseCard

--- src/components/exercise/ExerciseCard.js
+++ src/components/exercise/ExerciseCard.js
@@ -1,35 +1,36 @@
 import { Link } from "react-router-dom";
-import { Avatar, Badge, Box,  Card, CardBody, Flex, Grid,  Text, VStack } from "@chakra-ui/react";
+import { Card, CardBody, Flex, Grid, Text } from "@chakra-ui/react";
+import UserCard from "../user/UserCard";
+import Timestamp from "../../utils/timestamp";
 
 const ExerciseCard = (props) => {
-    const { index, userCode, userId, userName, name, content, createDate, modDate} = props.exercise;
+    const { index, userCode, userId, userName, name, content, createDate, modDate } = props.exercise;
 
     return (
-        <Card key={index} _hover={{backgroundColor: "gray.500"}} >
+        <Card key={index}>
             <CardBody>
-                <Grid templateColumns="repeat(2, 1fr)" p="10px" gap="10px" borderRadius="10px">
-                    <Box p="10px" borderRadius="10px" bgColor="gray.200" cursor="pointer"
-                        _hover={{backgroundColor: "gray.400"}} >
-                        <Link to={`/user/${userCode}`}>
-                            <Flex direction="column" gap="10px" align="center">
-                                <Avatar src="" size="2xl"/>
-                                <VStack gap="10px">
-                                    <Text>{userName}</Text>
-                                    <Badge fontSize="15px" colorScheme="pink">
-                                        {userId}
-                                    </Badge>
-                                </VStack>
-                            </Flex>
-                        </Link>
-                    </Box>
-                    <Link to={`/exercises/${index}`}>
-                        <Flex direction="column" gap="10px">
-                            <Text>운동 이름: {name}</Text>
-                            <Text>운동법: {content}</Text>
-                            <Text>작성일: {createDate}</Text>
-                            {modDate && <Text>수정일: {modDate}</Text> }
-                        </Flex>
-                    </Link>
+                <Grid templateColumns="repeat(2, 1fr)" h="100%" p="10px" gap="10px" borderRadius="10px">
+                    <UserCard user={{ code: userCode, id: userId, name: userName}} />
+                    <Card _hover={{backgroundColor: "gray.500"}}>
+                        <CardBody>
+                            <Link to={`/exercises/${index}`}>
+                                <Grid templateColumns="repeat(2, 1fr)" h="100%" p="10px" gap="10px" borderRadius="10px">
+                                    <Flex direction="column" gap="10px">
+                                        <Text>운동 이름</Text>
+                                        <Text>운동법</Text>
+                                        <Text>작성일</Text>
+                                        <Text>수정일</Text>
+                                    </Flex>
+                                    <Flex direction="column" gap="10px">
+                                        <Text>{name}</Text>
+                                        <Text>{content}</Text>
+                                        <Text>{Timestamp.convertToDate(createDate)}</Text>
+                                        {modDate && <Text>{Timestamp.convertToDate(modDate)}</Text> }
+                                    </Flex>
+                                </Grid>
+                            </Link>
+                        </CardBody>
+                    </Card>
                 </Grid>
             </CardBody>
         </Card>
