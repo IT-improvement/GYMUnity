4136e2f3b51bf0bca0ffd08520c546b2edd5795e
k0gang <k0gang@naver.com>
2024-06-07 10:20:44 +0900

    refactoring imageurl

--- src/components/diet/createFoodCategoryForm.js
+++ src/components/diet/createFoodCategoryForm.js
@@ -1,108 +1,94 @@
-import { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Input,
-  Image,
-  Stack,
-} from "@chakra-ui/react";
-import Context from "../../Context";
-import Toast from "../chakra/Toast";
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Box, Button, Flex, FormControl, FormLabel, Input, Image, Stack } from '@chakra-ui/react';
+import Context from '../../Context';
+import Toast from '../chakra/Toast';
+import FileUpload from '../feed/FileUpload';
 
 const CreateFoodCategoryForm = () => {
-  const [categoryName, setCategoryName] = useState("");
-  const [imageFile, setImageFile] = useState(null);
-  const { isLoggedIn, sessionUser } = useContext(Context);
-  const navigate = useNavigate();
+    const { isLoggedIn, sessionUser } = useContext(Context);
+    const navigate = useNavigate();
+    const [foodCategory, setFoodCategory] = useState({
+        categoryName: '',
+        image: null,
+    });
 
-  const handleCategoryNameChange = (e) => {
-    setCategoryName(e.target.value);
-  };
+    const handleCreateCategory = async (e) => {
+        e.preventDefault();
 
-  const handleImageChange = (e) => {
-    setImageFile(e.target.files[0]);
-  };
+        try {
+            console.log(foodCategory);
+            let isCreate = false;
+            const encodedCategoryName = encodeURIComponent(foodCategory.categoryName);
+            console.log(foodCategory.categoryName);
+            console.log(encodedCategoryName);
+            const response = await fetch(
+                `${process.env.REACT_APP_SERVER_URL}/foodCategory/service?command=createFoodCategory&categoryName=${encodedCategoryName}`,
+                {
+                    method: 'POST',
+                    headers: {
+                        Authorization: sessionUser.code,
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        image: foodCategory.image,
+                    }),
+                }
+            );
 
-  const handleCreateCategory = async (e) => {
-    e.preventDefault();
-
-    try {
-      const formData = new FormData();
-      formData.append("userCode", sessionUser.code);
-      formData.append("categoryName", categoryName);
-      formData.append("categoryImage", imageFile);
-
-      const response = await fetch(
-        `${process.env.REACT_APP_SERVER_URL}/foodCategory/service?command=createFoodCategory`,
-        {
-          method: "POST",
-          headers: {
-            Authorization: sessionUser.code,
-          },
-          body: formData,
+            const responseData = await response.json();
+            if (responseData.status === 200) {
+                Toast.show(true, '음식 카테고리 생성 완료', '음식 카테고리 생성 실패');
+                navigate('/diet/foodList');
+            } else {
+                Toast.showFailed('음식 카테고리 생성 실패');
+            }
+        } catch (error) {
+            console.error('Error creating Food Category:', error);
+            Toast.showFailed('Error creating Food Category');
         }
-      );
+    };
 
-      const responseData = await response.json();
-      if (responseData.status === 200) {
-        Toast.show(true, "음식 카테고리 생성 완료", "음식 카테고리 생성 실패");
-        navigate("/diet/foodList");
-      } else {
-        Toast.showFailed("음식 카테고리 생성 실패");
-      }
-    } catch (error) {
-      console.error("Error creating Food Category:", error);
-      Toast.showFailed("Error creating Food Category");
-    }
-  };
+    const handleImageFieldOnChange = (e) => {
+        setFoodCategory((oldImageFile) => {
+            return { ...oldImageFile, [e.target.name]: e.target.value };
+        });
+    };
 
-  useEffect(() => {
-    if (!isLoggedIn) navigate("/diet");
-  }, [isLoggedIn, navigate]);
+    const handleFileOnChange = (file) => {
+        setFoodCategory((oldImageFile) => {
+            return { ...oldImageFile, image: file };
+        });
+    };
 
-  return (
-    <Flex w="100%" justify="center">
-      <form
-        style={{ minWidth: "70%" }}
-        method="POST"
-        onSubmit={handleCreateCategory}
-      >
-        <FormControl>
-          <FormLabel>카테고리 이름</FormLabel>
-          <Input
-            required
-            type="text"
-            value={categoryName}
-            onChange={handleCategoryNameChange}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel>카테고리 이미지</FormLabel>
-          <Input type="file" accept="image/*" onChange={handleImageChange} />
-          {imageFile && (
-            <Image
-              src={URL.createObjectURL(imageFile)}
-              alt="Selected Image"
-              boxSize="150px"
-              objectFit="cover"
-            />
-          )}
-        </FormControl>
-        <Flex justify="space-between" mt={4}>
-          <Button colorScheme="blue" onClick={() => navigate(-1)}>
-            뒤로가기
-          </Button>
-          <Button type="submit" colorScheme="green">
-            카테고리 생성
-          </Button>
+    useEffect(() => {
+        if (!isLoggedIn) navigate('/diet');
+    }, [isLoggedIn, navigate]);
+
+    return (
+        <Flex w="100%" justify="center">
+            <form style={{ minWidth: '70%' }} method="POST" onSubmit={handleCreateCategory}>
+                <FormControl>
+                    <FormLabel>카테고리 이름</FormLabel>
+                    <Input
+                        name="categoryName"
+                        value={foodCategory.categoryName}
+                        type="text"
+                        onChange={handleImageFieldOnChange}
+                    />
+                </FormControl>
+                <FileUpload handleFileOnChange={handleFileOnChange}></FileUpload>
+                <Flex justify="space-between" mt={4}>
+                    <Button colorScheme="blue" onClick={() => navigate(-1)}>
+                        뒤로가기
+                    </Button>
+                    <Button type="submit" colorScheme="green">
+                        카테고리 생성
+                    </Button>
+                </Flex>
+            </form>
         </Flex>
-      </form>
-    </Flex>
-  );
+    );
 };
 
 export default CreateFoodCategoryForm;
